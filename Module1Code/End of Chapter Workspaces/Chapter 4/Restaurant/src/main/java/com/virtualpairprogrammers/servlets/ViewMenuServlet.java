50af5f191d2afc5d41fe00ac5545f3c0c300d170
RobertWAFowler <rfowler.reg@gmail.com>
2019-01-07 12:11:09 +0200

    23. GET and POST exercise - My solution

--- Module1Code/End of Chapter Workspaces/Chapter 4/Restaurant/src/main/java/com/virtualpairprogrammers/servlets/ViewMenuServlet.java
+++ Module1Code/End of Chapter Workspaces/Chapter 4/Restaurant/src/main/java/com/virtualpairprogrammers/servlets/ViewMenuServlet.java
@@ -1,34 +1,36 @@
 package com.virtualpairprogrammers.servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-import java.util.List;
+import com.virtualpairprogrammers.data.MenuDataService;
+import com.virtualpairprogrammers.domain.MenuItem;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.virtualpairprogrammers.data.MenuDataService;
-import com.virtualpairprogrammers.domain.MenuItem;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ViewMenuServlet extends HttpServlet {
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
-		
-		MenuDataService menuDataService = new MenuDataService();
-		List<MenuItem> menuItems = menuDataService.getFullMenu();
-		
-		out.println("<html><body><h1>Ricky's Restaurant</h1>");
-		out.println("<h2>Menu</h2><ul>");
-		for (MenuItem menuItem : menuItems) {
-			out.println("<li>" + menuItem + "</li>");
-		}
-		
-		out.println("</ul></body></html>");
-		out.close();
-	}
-	
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        MenuDataService menuDataService = new MenuDataService();
+        List<MenuItem> menuItems = menuDataService.getFullMenu();
+
+        out.println("<html><body><h1>Ricky's Restaurant</h1>");
+        out.println("<h2>Menu</h2><ul>");
+        out.println("<form action='/orderReceived.html' method='POST'>");
+
+        for (MenuItem menuItem : menuItems) {
+            out.println("<li><label>" + menuItem + "<input type='number' name='item_" + menuItem.getId() + "'/></label></li>");
+        }
+
+        out.println("<input type='submit'/>");
+        out.println("</form>");
+        out.println("</ul></body></html>");
+        out.close();
+    }
+
 }
